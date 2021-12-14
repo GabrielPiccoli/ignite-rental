fdd0e26b3078e681b7dd9ec6eaac2d25db0d7817
TBRWEB - Desenvolvimento de Sites <tbrweb01@gmail.com>
2021-12-14 17:42:05 -0300

    solid and continuation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,24 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+
+import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
-  const category = {
-    name,
-    description,
-    id: uuidV4(),
-  };
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  categories.push(category);
+  createCategoryService.execute({ name, description });
 
   return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoriesRepository.list();
+
+  return res.json(all);
 });
 
 export { categoriesRoutes };
