a7ab0d62bbd23ee2e08cbe965950e01271934bb5
Gabriel Piccoli <gabriel.pdmarcos@gmail.com>
2022-03-08 23:10:30 -0300

    continuação deploy

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUSerController } from "@modules/accounts/useCases/profileUser/profileUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 import uploadConfig from "../../../../config/upload";
@@ -13,6 +14,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUSerController();
 
 usersRoutes.post("/", createUserController.handle);
 usersRoutes.patch(
@@ -21,5 +23,6 @@ usersRoutes.patch(
   uploadAvatar.single("avatar"),
   updateUserAvatarController.handle
 );
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
